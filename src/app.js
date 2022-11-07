d8fd7d18724111305381d964ad743aaeebff88b9
Sylvia <88979694+shernandez253@users.noreply.github.com>
2022-11-07 09:45:39 -0500

    Formatted Date & Time

--- src/app.js
+++ src/app.js
@@ -8,6 +8,37 @@ function displayDescription(response) {
   description.innerHTML = response.data.weather[0].main;
 }
 
+function formatDateTime(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hour > 12) {
+    hour = `0${hour}`;
+  }
+
+  return `${weekDays[day]} ${hour}:${minutes}`;
+}
+
+function displayDateTime(response) {
+  let dateTime = document.querySelector("#date-time");
+  dateTime.innerHTML = formatDateTime(response.data.dt * 1000);
+}
+
 function displayFeelsLike(response) {
   let precipiation = document.querySelector("#feels-like");
   precipiation.innerHTML = response.data.main.humidity;
@@ -31,13 +62,13 @@ function displayTemperature(response) {
 
   displayCity(response);
   displayDescription(response);
-  //call to display date/time
+  displayDateTime(response);
   displayFeelsLike(response);
   displayHumidity(response);
   displayWind(response);
 }
 
-let cityName = "springfield";
+let cityName = "boston";
 let apiKey = "9c0a0dd5ce072e1ac8919092ab708dad";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 
